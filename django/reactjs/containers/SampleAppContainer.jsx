38a803cf3e202ab57773ebfe5572bf91678da5b4
Jared Ross <jaredrossj@gmail.com>
2018-01-27 19:25:34 -0500

    working aws

--- django/reactjs/containers/SampleAppContainer.jsx
+++ django/reactjs/containers/SampleAppContainer.jsx
@@ -1,25 +1,7 @@
 import React from "react"
 import Radium from "radium"
 
-import { connect } from "react-redux"
 
-import * as counterActions from "../actions/counterActions"
-import Headline from "../components/Headline"
-
-const styles = {
-  button: {
-    cursor: "pointer",
-  },
-  counter: {
-    color: "blue",
-    fontSize: "20px",
-  }
-}
-
-@connect(state => ({
-  counters: state.counters,
-}))
-@Radium
 export default class SampleAppContainer extends React.Component {
   handleClick() {
     let {dispatch} = this.props;
@@ -29,15 +11,64 @@ export default class SampleAppContainer extends React.Component {
   render() {
     let {counters} = this.props
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <Headline>Sample App!</Headline>
-            <div style={[styles.button]} onClick={() => this.handleClick()}>INCREASE</div>
-            <p style={[styles.counter]}>{counters.clicks}</p>
-            <p>{process.env.BASE_API_URL}</p>
+      <div classNameName="container">
+      <section>
+          <h1>Monthly Snapshot</h1>
+          <div className="spacing1">
+
           </div>
-        </div>
+
+          <div className = "transparentbox"> </div>
+          <div className="spacing1"> </div>
+
+          <div style={{text_align:'center'}}>
+              <a style={{text_decoration: 'none'}} className="transparent_btn" href="#">Do something with this button</a>
+              <div className="spacing1"> </div>
+              <a style={{text_decoration:"none"}} href="#hidden-div" data-target="#hidden-div" className="transparent_btn" data-toggle="collapse">This is the button that shows collapsed elements</a>
+              <div id ="hidden-div" className ="collapse" style={{color:"black"}}>
+                  <div className="spacing1"> </div>
+                  Place search/ whatever in here
+
+              </div>
+
+
+
+          </div>
+
+          <div className="spacing1"> </div>
+          <div className="spacing1"> </div>
+          <div className="spacing1"> </div>
+          <div className="spacing1"> </div>
+
+          <h1>Table Title</h1>
+          <div className="spacing1"> </div>
+
+          <div className="tbl-header">
+            <table cellpadding="0" cellspacing="0" border="0">
+              <thead>
+                <tr>
+                  <th>Client</th>
+                  <th>Logo</th>
+                  <th>Location</th>
+                  <th>Kiosk </th>
+                  <th>Total Charge</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+          <div className="tbl-content">
+            <table cellpadding="0" cellspacing="0" border="0">
+              <tbody>
+              </tbody>
+            </table>
+          </div>
+          <div className="spacing1"> </div>
+          <a className="transparent_btn" href="#">Add</a>
+        </section>
+
+        <div className="spacing1"> </div>
+        <div className="spacing1"> </div><div className="spacing1"> </div><div className="spacing1"> </div>
+
       </div>
     )
   }
