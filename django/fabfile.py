4d98dad63f71d396b6e12ee64f522c00baf177dc
Jared Ross <jaredrossj@gmail.com>
2018-02-13 02:53:30 -0500

    deploy

--- django/fabfile.py
+++ django/fabfile.py
@@ -3,5 +3,7 @@ from fabric.api import local
 def webpack():
     local('rm -rf djreact/static/bundles/stage/*')
     local('rm -rf djreact/static/bundles/prod/*')
+    local('rm -rf djreact/static/bundles/local/*')
     local('webpack --config webpack.stage.config.js --progress --colors')
+    local('webpack --config webpack.local.config.js --progress --colors')
     local('webpack --config webpack.prod.config.js --progress --colors')
