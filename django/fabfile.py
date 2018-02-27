fe466a9e46a112829029b8c63db72f431ddbca76
Jared Ross <jaredrossj@gmail.com>
2018-02-27 17:00:00 -0500

    no crazy webpakc

--- django/fabfile.py
+++ django/fabfile.py
@@ -1,9 +1,9 @@
 from fabric.api import local
 
 def webpack():
-    local('rm -rf djreact/static/bundles/stage/*')
-    local('rm -rf djreact/static/bundles/prod/*')
-    local('rm -rf djreact/static/bundles/local/*')
+    local('rm -rf www/static/bundles/stage/*')
+    local('rm -rf wwww/static/bundles/prod/*')
+    local('rm -rf www/static/bundles/local/*')
     local('webpack --config webpack.stage.config.js --progress --colors')
     local('webpack --config webpack.local.config.js --progress --colors')
     local('webpack --config webpack.prod.config.js --progress --colors')
